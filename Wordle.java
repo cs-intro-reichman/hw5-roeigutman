6889de608389e18a617b383e57232e90563cb090
roei gutman <git config --global user.email david.german@post.runi.ac.il>
2025-11-29 14:50:57 +0200

    Completed assignment

--- Wordle.java
+++ Wordle.java
@@ -55,7 +55,7 @@ public class Wordle {
 
                 resultRow[i] = 'Y'; 
             
-            } else  { resultRow[i] = '-';
+            } else  { resultRow[i] = '_';
              
         }                   
 
